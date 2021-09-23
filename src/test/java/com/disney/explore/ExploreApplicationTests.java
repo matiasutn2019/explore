184902a2fb70f83b473fb13365709930326649b6
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-09-23 16:26:10 -0300

    first commit

---
+++ src/test/java/com/disney/explore/ExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
