a2c8f17f04c0f59db95a49d0b221eaa84d3065d6
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-09-27 16:19:07 -0300

    user controller

---
+++ src/main/java/com/disney/explore/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.disney.explore.controller;
+
+import com.disney.explore.domain.AppUser;
+import com.disney.explore.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AppUser> registrarUser(@RequestBody AppUser user) throws Exception {
+        userService.registrarUser(user);
+        return ResponseEntity.ok().build();
+    }
+}
