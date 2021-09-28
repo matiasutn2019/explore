885683dc8bb5cb16b085d621a3e66ce7317ec766
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-09-28 15:01:55 -0300

    roles created

--- src/main/java/com/disney/explore/ExploreApplication.java
+++ src/main/java/com/disney/explore/ExploreApplication.java
@@ -1,7 +1,16 @@
 package com.disney.explore;
 
+import com.disney.explore.domain.AppUser;
+import com.disney.explore.domain.Role;
+import com.disney.explore.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class ExploreApplication {
@@ -10,4 +19,16 @@ public class ExploreApplication {
 		SpringApplication.run(ExploreApplication.class, args);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+		};
+	}
 }
