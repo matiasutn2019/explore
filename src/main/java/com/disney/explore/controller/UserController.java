22cdabc9234cd4081133b3209b8446119d551566
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-10-03 10:07:20 -0300

    save role & /movies authorizations added

--- src/main/java/com/disney/explore/controller/UserController.java
+++ src/main/java/com/disney/explore/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.disney.explore.controller;
 
 import com.disney.explore.domain.AppUser;
+import com.disney.explore.domain.Role;
 import com.disney.explore.service.UserService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,4 +26,21 @@ public class UserController {
         userService.registerUser(user);
         return ResponseEntity.ok().build();
     }
+
+    @PostMapping("/addroletouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/saverole")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/auth/saverole").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+}
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
