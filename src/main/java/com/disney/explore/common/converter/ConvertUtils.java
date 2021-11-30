12f8aebb8cd30cbf1e14343dca111c15f7558bdd
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-30 15:05:39 -0300

    explore: convert methods performed

--- src/main/java/com/disney/explore/common/converter/ConvertUtils.java
+++ src/main/java/com/disney/explore/common/converter/ConvertUtils.java
@@ -19,50 +19,81 @@ import org.springframework.stereotype.Component;
 @Component("convertUtils")
 public class ConvertUtils {
 
-  public UserAuthenticatedResponse toUserAuthenticatedResponse(String email, String token) {
-      UserAuthenticatedResponse userAuthenticatedResponse = new UserAuthenticatedResponse();
-      userAuthenticatedResponse.setEmail(email);
-      userAuthenticatedResponse.setToken(token);
-      return userAuthenticatedResponse;
-  }
+    public UserAuthenticatedResponse toUserAuthenticatedResponse(String email, String token) {
+        UserAuthenticatedResponse userAuthenticatedResponse = new UserAuthenticatedResponse();
+        userAuthenticatedResponse.setEmail(email);
+        userAuthenticatedResponse.setToken(token);
+        return userAuthenticatedResponse;
+    }
 
-  public UserCreatedResponse toUserCreatedResponse(User user) {
-      UserCreatedResponse userCreatedResponse = new UserCreatedResponse();
-      userCreatedResponse.setId(user.getId());
-      userCreatedResponse.setEmail(user.getEmail());
-      return userCreatedResponse;
-  }
+    public UserCreatedResponse toUserCreatedResponse(User user) {
+        UserCreatedResponse userCreatedResponse = new UserCreatedResponse();
+        userCreatedResponse.setId(user.getId());
+        userCreatedResponse.setEmail(user.getEmail());
+        return userCreatedResponse;
+    }
 
-  private CharacterResponse toCharacterResponse(Character character) {
-    return null;
-  }
 
-  public CharacterResponseList toCharacterResponseList(List<Character> characters) {
-    return null;
-  }
+    private CharacterResponse toCharacterResponse(Character character) {
+        CharacterResponse characterResponse = new CharacterResponse();
+        characterResponse.setNombre(character.getNombre());
+        characterResponse.setImage(character.getImage());
+        return characterResponse;
+    }
 
-  public CharacterResponseDetail toCharacterResponseDetail(Character characters) {
-    return null;
-  }
+    public CharacterResponseList toCharacterResponseList(List<Character> characters) {
+        CharacterResponseList characterResponseList = new CharacterResponseList();
+        characters.forEach(character -> characterResponseList.getCharacterResponseList().add(toCharacterResponse(character)));
+        return characterResponseList;
+    }
 
-  public CharacterResponseDetailList toCharacterResponseDetailList(List<Character> characters) {
-    return null;
-  }
+    public CharacterResponseDetail toCharacterResponseDetail(Character character) {
+        CharacterResponseDetail characterResponseDetail = new CharacterResponseDetail();
+        characterResponseDetail.setId(character.getId());
+        characterResponseDetail.setNombre(character.getNombre());
+        characterResponseDetail.setImage(character.getImage());
+        characterResponseDetail.setEdad(character.getEdad());
+        characterResponseDetail.setPeso(character.getPeso());
+        characterResponseDetail.setHistoria(character.getHistoria());
+        characterResponseDetail.setPeliculas(character.getPeliculas());
+        return characterResponseDetail;
+    }
 
-  private MovieResponse toMovieResponse(Movie movie) {
-    return null;
-  }
+    public CharacterResponseDetailList toCharacterResponseDetailList(List<Character> characters) {
+        CharacterResponseDetailList characterResponseDetailList = new CharacterResponseDetailList();
+        characters.forEach(character -> characterResponseDetailList.getCharacterResponseDetailList().add(toCharacterResponseDetail(character)));
+        return characterResponseDetailList;
+    }
 
-  public MovieResponseList toMovieResponseList(List<Movie> movies) {
-    return null;
-  }
 
-  public MovieResponseDetail toMovieResponseDetail(Movie movie) {
-    return null;
-  }
+    private MovieResponse toMovieResponse(Movie movie) {
+        MovieResponse movieResponse = new MovieResponse();
+        movieResponse.setImage(movie.getImage());
+        movieResponse.setTitulo(movie.getTitulo());
+        movieResponse.setFechaCreacion(movie.getFechaCreacion());
+        return movieResponse;
+    }
 
-  public MovieResponseDetailList toMovieResponseDetailList(List<Movie> movie) {
-    return null;
-  }
+    public MovieResponseList toMovieResponseList(List<Movie> movies) {
+        MovieResponseList movieResponseList = new MovieResponseList();
+        movies.forEach(movie -> movieResponseList.getMovieResponseList().add(toMovieResponse(movie)));
+        return movieResponseList;
+    }
+
+    public MovieResponseDetail toMovieResponseDetail(Movie movie) {
+        MovieResponseDetail movieResponseDetail = new MovieResponseDetail();
+        movieResponseDetail.setId(movie.getId());
+        movieResponseDetail.setTitulo(movie.getTitulo());
+        movieResponseDetail.setImage(movie.getImage());
+        movieResponseDetail.setFechaCreacion(movie.getFechaCreacion());
+        movieResponseDetail.setPersonajes(movie.getPersonajes());
+        return movieResponseDetail;
+    }
+
+    public MovieResponseDetailList toMovieResponseDetailList(List<Movie> movies) {
+        MovieResponseDetailList movieResponseDetailList = new MovieResponseDetailList();
+        movies.forEach(movie -> movieResponseDetailList.getMovieResponseDetailList().add(toMovieResponseDetail(movie)));
+        return movieResponseDetailList;
+    }
 
 }
