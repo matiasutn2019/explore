b0dd75830beef26c533974cddc7e256d0d78a76f
Matias <matiasceviniydejesus@gmail.comgit config --global user.email matiasceviniydejesus@gmail.com>
2021-11-30 16:51:37 -0300

    explore: param validation added

---
+++ src/main/java/com/disney/explore/common/validation/ValidationMessages.java
@@ -0,0 +1,8 @@
+package com.disney.explore.common.validation;
+
+public class ValidationMessages {
+
+  public static final String REQUEST_PARAM_RANGE_ERROR_MESSAGE =
+      "The %s attribute must be more than {min} characters and must not be more than {max} characters";
+
+}
--- src/main/java/com/disney/explore/domain/request/MovieRequest.java
+++ src/main/java/com/disney/explore/domain/request/MovieRequest.java
@@ -1,7 +1,9 @@
 package com.disney.explore.domain.request;
 
+import com.disney.explore.common.validation.ValidationMessages;
 import com.disney.explore.domain.entity.Character;
 import java.util.List;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +18,7 @@ public class MovieRequest {
   private String image;
   private String titulo;
   private String fechaCreacion;
+  @Size(min = 1, max = 5, message = ValidationMessages.REQUEST_PARAM_RANGE_ERROR_MESSAGE)
   private Integer calificacion;
   private List<Character> personajes;
 
